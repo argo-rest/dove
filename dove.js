5e77d5b4848c3c251499fbb68f694b465b5f751b
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-04-17 12:51:09 +0100

    Initial import

---
+++ dove.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var System = require('systemjs');
+require('./config');
+
+var uriArgument = process.argv[2];
+
+// FIXME: hack to allow passing header but no URL
+if (uriArgument === '-') {
+  uriArgument = undefined;
+}
+
+// TODO: or better, receive headers as CLI flags
+var headerKey = process.argv[3];
+var headerValue = process.argv[4];
+
+var headers = {};
+if (headerKey && headerValue) {
+  headers[headerKey] = headerValue;
+}
+
+
+System.import('./lib/core').then(function(core) {
+  var execution;
+  if (uriArgument) {
+    execution = core.run(uriArgument, headers);
+  } else {
+    execution = core.promptForUri().then(function(uri) {
+      return core.run(uri, headers);
+    });
+  }
+
+  execution.then(function() {
+    process.exit(0);
+  }).catch(function(error) {
+    console.error(error.stack);
+    process.exit(1);
+  });
+});
